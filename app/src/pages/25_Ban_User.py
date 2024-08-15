e4caf80d3f8c70019d5e2a29185e98777b88c099
Sadfahlsdj's mac <124646333+Sadfahlsdj@users.noreply.github.com>
2024-08-15 13:26:43 -0400

    added confirmation box to ban_user so erroneous bans don't happen

--- app/src/pages/25_Ban_User.py
+++ app/src/pages/25_Ban_User.py
@@ -33,12 +33,16 @@ user_input = st.selectbox(
     placeholder="Select user...",
 )
 
-# Check if the user has entered something
-if user_input:
+confirmed_user = None
+if st.button('Confirm Selection'):
+    confirmed_user = user_input
+    st.write(f'Selected user {confirmed_user}')
 
-    username = user_input
+# Check if the user has entered something
+if confirmed_user:
+
     #Construct API URL
-    api_url = f'http://web-api:4000/ma/admins/ban/{username}'
+    api_url = f'http://web-api:4000/ma/admins/ban/{confirmed_user}'
    
     try: 
         # Make a POST request to the API to follow artist
@@ -46,9 +50,9 @@ if user_input:
 
         #Check if request was successful
         if response.status_code == 200:
-            st.write(f"Successfully deleted {username}")
+            st.write(f"Successfully deleted {confirmed_user}")
         else:
-            st.write(f"Failed to delete {user_input}. Status Code: {response.status_code}")
+            st.write(f"Failed to delete {confirmed_user}. Status Code: {response.status_code}")
     except requests.exceptions.RequestException as e:
         st.write('could not connect to database to delete user!')
         logger.error(f"Error occurred: {e}")
